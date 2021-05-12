14bd3c52501adbbf57e5d477a8ff7817e248e05b
xykwin <kunghsu@qq.com>
2021-05-12 21:40:01 +0800

    注释完善

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
+/**
+ * 这是入口文件，webpack会指定这个文件，指定的位置在：webpack.base.config.js
+ */
 import Vue from 'vue';
 import ViewUI from 'view-design';
 import VueRouter from 'vue-router';
-import Routers from './router';
+import Routers from './router';//自定义路由，也是iview demo工程自动提供的
 import Util from './libs/util';
-import App from './app.vue';
+import App from './app.vue';//引入首页组件，表示网站的入口
 import 'view-design/dist/styles/iview.css';
 
-Vue.use(VueRouter);
-Vue.use(ViewUI);
+Vue.use(VueRouter);//加载路由组件
+Vue.use(ViewUI);//加载ViewUI组件
 
 // 路由配置
 const RouterConfig = {
