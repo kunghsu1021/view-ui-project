728151c49fc222f5cd4d90f2c9bd5875150c47fa
xuyaokun_kzx <xuyaokun_kzx@alibaba.com>
2021-06-02 17:22:11 +0800

    继续改进list demo

--- src/views/demo/layout/demo-list.vue
+++ src/views/demo/layout/demo-list.vue
@@ -4,7 +4,7 @@
     1.步骤名
     2.多选框，选发送目标 -->
     <List item-layout="vertical">
-        <ListItem v-for="(item, index) in data" :key="index" >
+        <ListItem v-for="(item, index) in listData" :key="index" >
             <ListItemMeta  :title="item.title" />
 <!--            
      {{ item.content }}
@@ -17,18 +17,22 @@
             :value="item.checkAll"
             @click.prevent.native="handleCheckAll(index)">全选</Checkbox>
     </div>
-    <CheckboxGroup v-model="item.checkAllGroup" @on-change="checkAllGroupChange">
+    <!-- 下面的on-change事件函数，假如不传参，默认会把选中的数组返回，假如传参，则不返回数组，返回的是你指定的参数 -->
+    <CheckboxGroup v-model="item.checkAllGroup" @on-change="checkAllGroupChange(item.checkAllGroup, index)" >
 <!--         
         <Checkbox label="香蕉"></Checkbox>
         <Checkbox label="苹果"></Checkbox>
         <Checkbox label="西瓜"></Checkbox> 
         :value="item.id"
         -->
-         <Checkbox v-for="item2 in item.checkArray" :key="item2.id"  :label="item2.name"></Checkbox>
+         <Checkbox v-for="item2 in item.checkArray" :key="item2.id"  :label="item2.name" border></Checkbox>
     </CheckboxGroup>
     </template>
         </ListItem>
     </List>
+
+                     <Button @click="addStep">添加步骤</Button>
+                    <Button @click="deleteStep">删除步骤</Button>
 
 </div>
 </template>
@@ -36,13 +40,16 @@
     export default {
         data () {
             return {
-                
-                data: [
+                sourceCheckArray:[
+                            {id:"dx", name:"短信"},
+                            {id:"wx", name:"微信"},
+                            {id:"zfb", name:"支付宝"},
+                            {id:"dy", name:"电邮"},
+                            {id:"sjqq", name:"手机QQ"}
+                        ],
+                listData: [ //展示List元素的数据源
                     {
                         title: '步骤1',
-                        description: 'This is description, this is description, this is description.',
-                        avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
-                        content: 'This is the content, this is the content, this is the content, this is the content.',
                         checkArray: [
                             {id:"dx", name:"短信"},
                             {id:"wx", name:"微信"},
@@ -56,9 +63,6 @@
                     },
                     {
                         title: '步骤2',
-                        description: 'This is description, this is description, this is description.',
-                        avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
-                        content: 'This is the content, this is the content, this is the content, this is the content.',
                         checkArray: [
                             {id:"dx", name:"短信"},
                             {id:"wx", name:"微信"},
@@ -72,9 +76,6 @@
                     },
                     {
                         title: '步骤3',
-                        description: 'This is description, this is description, this is description.',
-                        avatar: 'https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar',
-                        content: 'This is the content, this is the content, this is the content, this is the content.',
                         checkArray: [
                             {id:"dx", name:"短信"},
                             {id:"wx", name:"微信"},
@@ -114,17 +115,77 @@
                     this.data[index].checkAllGroup = [];
                 }
             },
-            checkAllGroupChange (data) {//多选框选中事件
-                // if (data.length === 3) {
-                //     this.indeterminate = false;
-                //     this.checkAll = true;
-                // } else if (data.length > 0) {
-                //     this.indeterminate = true;
-                //     this.checkAll = false;
-                // } else {
-                //     this.indeterminate = false;
-                //     this.checkAll = false;
-                // }
+            checkAllGroupChange (value,index) {//多选框选中事件
+                console.log("checkAllGroupChange");
+                console.log(value);
+                console.log(index);
+
+                var listDataItem = this.listData[index];
+                //获取已选中的值，是一个数组
+                var checkAllGroup = listDataItem.checkAllGroup;
+                console.log(checkAllGroup);
+
+                //因为上一步骤选中的元素，不能出现到下一个步骤中，所以需要从头开始遍历整个数组，逐个修改 checkArray 和 checkAllGroup
+                //先用一个数组，记录已经被选中的项
+                var hasChooseArray = [];
+                // debugger;
+                for(var i = 0; i < this.listData.length; i++) {
+                     //获取当前的可选项
+                    var checkArray = this.listData[i].checkArray;
+
+                    //获取当前步骤所选中的值
+                    if(hasChooseArray.length == 0){
+                        //假如是零，说明当前是第一步或者上一步根本没勾选任何值，不用做处理
+                        console.log(checkArray);
+                    }else{
+                        var newcheckArray = [];
+                        checkArray.forEach(element => {
+                            if(!hasChooseArray.includes(element.name)){
+                                newcheckArray.push(element);
+                            }
+                        });
+                        //将得到的新数组，赋予给当前的可选项数组
+                        this.listData[i].checkArray = newcheckArray;
+
+                        console.log("循环处理弟" + i + "个元素");
+                        console.log(newcheckArray);
+                    }
+                         //将当前步骤的选中项放到hasChooseArray中，给下一步骤判断
+                    hasChooseArray = hasChooseArray.concat(this.listData[i].checkAllGroup);
+
+                    //遍历源数组，获取hasChooseArray中不存在的，且this.listData[i].checkArray也不存在的，要进行回添
+                    var needToAddArray = [];
+                    var currentCheckArray = this.listData[i].checkArray;
+                    this.sourceCheckArray.forEach(element => {
+                            // debugger;
+                            //currentCheckArray是一个对象数组
+                            var isExist = false;
+                            for(var k = 0; k < currentCheckArray.length; k++) {
+                                if(currentCheckArray[k].name == element.name){
+                                    isExist = true;
+                                    break;
+                                }
+                            }
+                            if(isExist){
+                                //
+                                return ;
+                            }
+                            //hasChooseArray是一个字符串数组
+                            if(hasChooseArray.includes(element.name)){
+                                return;
+                            }
+                               //假如sourceCheckArray中的元素，在hasChooseArray和checkArray都不存在，则重新添加2
+                                needToAddArray.push(element);
+                    });
+                   console.log("needToAddArray" + needToAddArray);
+                   this.listData[i].checkArray = this.listData[i].checkArray.concat(needToAddArray);
+                }
+            },
+            addStep() {
+                
+            },
+            deleteStep() {
+
             }
         }
     }
